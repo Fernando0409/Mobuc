211d959442022cc7b6ab53f657335bc27d4dff03
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-12-24 02:39:02 -0600

    Update config.php

--- MoBuc/admin/php/config.php
+++ MoBuc/admin/php/config.php
@@ -1,9 +1,4 @@
 <?php
-    $host = '127.0.0.1';
-    $user = 'postgres';
-    $password = 'DeveloperFer0405';
-    $db= 'prueba_DB';
-
     // Hacerlo con postgres
-    $conn = pg_connect('host = localhost dbname = mobuc user = fernando password = DeveloperFer0405')
+    $conn = pg_connect('host = localhost dbname =  user =  password = ')
         or die('No se pude conectar');
